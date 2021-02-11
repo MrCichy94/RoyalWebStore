638903d7c6017d44419fca7e38b06986aec31576
MrCichy94 <przemyslav.cichy@gmail.com>
2021-02-11 14:56:37 +0100

    2021_C1_Upload empty project

---
+++ src/main/java/pl/cichy/RoyalWebStore/RoyalWebStoreApplication.java
@@ -0,0 +1,13 @@
+package pl.cichy.RoyalWebStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalWebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalWebStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cichy/RoyalWebStore/RoyalWebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cichy.RoyalWebStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalWebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
