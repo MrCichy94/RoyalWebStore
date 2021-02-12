e2748206e98b8fb53616f3c345e0857afb2a8314
MrCichy94 <przemyslav.cichy@gmail.com>
2021-02-12 15:45:31 +0100

    2021_C10_Add_product_unitTest_for_initializing_data_in_argConstructor

--- src/test/java/pl/cichy/RoyalWebStore/model/ProductTest.java
+++ src/test/java/pl/cichy/RoyalWebStore/model/ProductTest.java
@@ -20,8 +20,24 @@ class ProductTest {
     @Test
     @DisplayName("should create product object with args")
     void should_create_product_obj_with_args(){
-        Product eg3 = new Product(1,"First", new BigDecimal("9.99"), new BigDecimal("0.23"));
+        Product eg2 = new Product(1,"First", new BigDecimal("9.99"), new BigDecimal("0.23"));
 
-        assertEquals("First", eg3.productName);
+        assertEquals("First", eg2.productName);
+    }
+
+    @Test
+    @DisplayName("should initialize properly product vat")
+    void should_properly_count_vatValue(){
+        Product eg3 = new Product(1,"First", new BigDecimal("200.00"), new BigDecimal("0.10"));
+
+        assertEquals(new BigDecimal("20.00"), eg3.vatValue);
+    }
+
+    @Test
+    @DisplayName("should initialize properly product base net price")
+    void should_properly_count_baseNetPrice(){
+        Product eg4 = new Product(1,"First", new BigDecimal("200.00"), new BigDecimal("0.10"));
+
+        assertEquals(new BigDecimal("180.00"), eg4.baseNetPrice);
     }
 }
