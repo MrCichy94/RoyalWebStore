693bbd60ef3052c1c007df12eeaedd614839c05b
MrCichy94 <przemyslav.cichy@gmail.com>
2021-02-17 00:22:51 +0100

    2021_C28_Add_address_model

--- src/main/java/pl/cichy/RoyalWebStore/model/Address.java
+++ src/main/java/pl/cichy/RoyalWebStore/model/Address.java
@@ -1,4 +1,60 @@
 package pl.cichy.RoyalWebStore.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
 public class Address {
+
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    int addressId;
+
+    @Size(max = 25)
+    String country;
+
+    @Size(min = 3, max = 25)
+    String city;
+
+    @Size(min = 3, max = 25)
+    String voivodeship;
+
+    @Size(max = 25)
+    String county;
+
+    @Size(min = 3, max = 25)
+    String zipCode;
+
+    @Size(min = 3, max = 25)
+    String streetName;
+
+    @Size(min = 1, max = 25)
+    String doorNumber;
+
+    @Size(max = 25)
+    String localNumber;
+
+    public Address() {
+    }
+
+    public Address(int addressId, String city, String voivodeship, String zipCode,
+                   String streetName, String doorNumber) {
+        this.addressId = addressId;
+        this.city = city;
+        this.voivodeship = voivodeship;
+        this.zipCode = zipCode;
+        this.streetName = streetName;
+        this.doorNumber = doorNumber;
+    }
 }
--- src/main/java/pl/cichy/RoyalWebStore/model/Customer.java
+++ src/main/java/pl/cichy/RoyalWebStore/model/Customer.java
@@ -1,4 +1,24 @@
 package pl.cichy.RoyalWebStore.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    int customerId;
+
+
 }
