db6e1963878f8ea36cc54e713623117a0602411e
MrCichy94 <przemyslav.cichy@gmail.com>
2021-02-17 00:09:26 +0100

    2021_C27_Add_contact_model

--- src/main/java/pl/cichy/RoyalWebStore/model/Contact.java
+++ src/main/java/pl/cichy/RoyalWebStore/model/Contact.java
@@ -1,4 +1,48 @@
 package pl.cichy.RoyalWebStore.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contacts")
 public class Contact {
+
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    int contactId;
+
+    @Pattern(regexp="(^$|[0-9]{9})")
+    String phoneNumber1;
+
+    @Pattern(regexp="(^$|[0-9]{9})")
+    String phoneNumber2;
+
+    @Size(max = 50)
+    String faxAddress;
+
+    @Size(max = 50)
+    String emailAddress;
+
+    @Size(max = 50)
+    String wwwDomain;
+
+    public Contact() {
+    }
+
+    public Contact(int contactId, String phoneNumber1, String emailAddress) {
+        this.contactId = contactId;
+        this.phoneNumber1 = phoneNumber1;
+        this.emailAddress = emailAddress;
+    }
 }
