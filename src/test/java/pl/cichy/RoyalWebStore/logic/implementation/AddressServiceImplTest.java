b3c01713b61cacc7b87e66456a87ce08a79351e2
MrCichy94 <przemyslav.cichy@gmail.com>
2021-02-24 17:53:05 +0100

    2021_C49_Create_addressServiceImpl_tests

---
+++ src/test/java/pl/cichy/RoyalWebStore/logic/implementation/AddressServiceImplTest.java
@@ -0,0 +1,117 @@
+package pl.cichy.RoyalWebStore.logic.implementation;
+
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pl.cichy.RoyalWebStore.model.Address;
+import pl.cichy.RoyalWebStore.model.repository.AddressRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AddressServiceImplTest {
+
+    @Test
+    @DisplayName("Should throw ResourceNotFoundException when we got address with already existing the same city" +
+            "and street name and door number")
+    void createNewAddressIfPossible_ThrowResourceNotFoundException() {
+        //given
+        var mockAddress = mock(Address.class);
+        //and
+        var mockAddressRepository = mock(AddressRepository.class);
+        when(mockAddressRepository
+                .findByDoorNumber(mockAddress.getCity()))
+                .thenReturn(Optional.of(mockAddress));
+        when(mockAddressRepository
+                .findByStreetName(mockAddress.getStreetName()))
+                .thenReturn(Optional.of(mockAddress));
+        when(mockAddressRepository
+                .findByCity(mockAddress.getDoorNumber()))
+                .thenReturn(Optional.of(mockAddress));
+        //system under test
+        var toTest = new AddressServiceImpl(mockAddressRepository);
+        //when
+        var exception = catchThrowable(() -> toTest.createNewAddressIfPossible(mockAddress));
+        //then
+        assertThat(exception).isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining("Account with this");
+    }
+
+    @Test
+    @DisplayName("Should create a new address if door number is diffrent")
+    void createNewAddressIfPossible_work_properly_cif_door_number_is_diffrent() {
+        //given
+        var mockAddress = mock(Address.class);
+        //and
+        var mockAddressRepository = mock(AddressRepository.class);
+        when(mockAddressRepository
+                .findByDoorNumber(mockAddress.getCity()))
+                .thenReturn(Optional.empty());
+        when(mockAddressRepository
+                .findByStreetName(mockAddress.getStreetName()))
+                .thenReturn(Optional.of(mockAddress));
+        when(mockAddressRepository
+                .findByCity(mockAddress.getDoorNumber()))
+                .thenReturn(Optional.of(mockAddress));
+        //system under test
+        var toTest = new AddressServiceImpl(mockAddressRepository);
+        //when
+        //then
+        Assertions.assertThatCode(() -> toTest.createNewAddressIfPossible(mockAddress))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Should create a new address if street name is diffrent")
+    void createNewAddressIfPossible_work_properly_if_street_name_is_diffrent() {
+        //given
+        var mockAddress = mock(Address.class);
+        //and
+        var mockAddressRepository = mock(AddressRepository.class);
+        when(mockAddressRepository
+                .findByDoorNumber(mockAddress.getCity()))
+                .thenReturn(Optional.of(mockAddress));
+        when(mockAddressRepository
+                .findByStreetName(mockAddress.getStreetName()))
+                .thenReturn(Optional.empty());
+        when(mockAddressRepository
+                .findByCity(mockAddress.getDoorNumber()))
+                .thenReturn(Optional.of(mockAddress));
+        //system under test
+        var toTest = new AddressServiceImpl(mockAddressRepository);
+        //when
+        //then
+        Assertions.assertThatCode(() -> toTest.createNewAddressIfPossible(mockAddress))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("Should create a new address if city is diffrent")
+    void createNewAddressIfPossible_work_properly_if_city_is_diffrent() {
+        //given
+        var mockAddress = mock(Address.class);
+        //and
+        var mockAddressRepository = mock(AddressRepository.class);
+        when(mockAddressRepository
+                .findByDoorNumber(mockAddress.getCity()))
+                .thenReturn(Optional.of(mockAddress));
+        when(mockAddressRepository
+                .findByStreetName(mockAddress.getStreetName()))
+                .thenReturn(Optional.of(mockAddress));
+        when(mockAddressRepository
+                .findByCity(mockAddress.getDoorNumber()))
+                .thenReturn(Optional.empty());
+        //system under test
+        var toTest = new AddressServiceImpl(mockAddressRepository);
+        //when
+        //then
+        Assertions.assertThatCode(() -> toTest.createNewAddressIfPossible(mockAddress))
+                .doesNotThrowAnyException();
+    }
+
+}
