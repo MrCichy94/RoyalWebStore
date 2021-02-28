e5bc9143f78715574cb7c930d3d81f5bbd15e46b
MrCichy94 <przemyslav.cichy@gmail.com>
2021-02-28 22:54:57 +0100

    2021_C69_Create_orderServiceImpl_test2_addToOrder_method

--- src/test/java/pl/cichy/RoyalWebStore/logic/implementation/OrderServiceImplTest.java
+++ src/test/java/pl/cichy/RoyalWebStore/logic/implementation/OrderServiceImplTest.java
@@ -3,8 +3,10 @@ package pl.cichy.RoyalWebStore.logic.implementation;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pl.cichy.RoyalWebStore.model.Copy;
 import pl.cichy.RoyalWebStore.model.Customer;
 import pl.cichy.RoyalWebStore.model.Order;
+import pl.cichy.RoyalWebStore.model.repository.CopyRepository;
 import pl.cichy.RoyalWebStore.model.repository.CustomerRepository;
 import pl.cichy.RoyalWebStore.model.repository.OrderRepository;
 
@@ -33,6 +35,26 @@ class OrderServiceImplTest {
         //then
         assertThat(exception).isInstanceOf(ResourceNotFoundException.class)
                 .hasMessageContaining("No customer found");
+    }
+
+    @Test
+    @DisplayName("Should throw ResourceNotFoundException when we dont got order with given order's ID")
+    void addToOrder_ThrowResourceNotFoundException() {
+        //given
+        var mockOrderRepository = mock(OrderRepository.class);
+        when(mockOrderRepository.existsById(1)).thenReturn(false);
+        //and
+        var mockCopy = mock(Copy.class);
+        var mockOrder = mock(Order.class);
+        //and
+        var mockCopyRepository = mock(CopyRepository.class);
+        //system under test
+        var toTest = new OrderServiceImpl(mockOrderRepository, null, mockCopyRepository);
+        //when
+        var exception = catchThrowable(() -> toTest.addToOrder(mockOrder.getOrderId(), mockCopy.getCopyId()));
+        //then
+        assertThat(exception).isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining("No order found");
     }
 
 }
