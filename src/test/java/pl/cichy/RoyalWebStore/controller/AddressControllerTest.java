9295093a99aa2d918f3a2d50eb511fe52503e026
MrCichy94 <przemyslav.cichy@gmail.com>
2021-03-23 22:31:45 +0100

    2021_C101_Add_TESTS_for_AddressController_method_POST_and_add_Transactional_annotation_of_not_writing_data_to_DB

--- src/test/java/pl/cichy/RoyalWebStore/controller/AddressControllerTest.java
+++ src/test/java/pl/cichy/RoyalWebStore/controller/AddressControllerTest.java
@@ -1,14 +1,13 @@
 package pl.cichy.RoyalWebStore.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.internal.bytebuddy.matcher.ElementMatchers;
-import org.assertj.core.util.Lists;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,13 +16,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import pl.cichy.RoyalWebStore.model.Address;
 import pl.cichy.RoyalWebStore.model.repository.AddressRepository;
 
-import java.util.List;
+import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,6 +32,7 @@ class AddressControllerTest {
     private AddressRepository addressRepository;
 
     @Test
+    @Transactional
     @DisplayName("should get single address")
     void readAddressById_shouldGetSingleAddress() throws Exception {
         //given
@@ -60,6 +56,7 @@ class AddressControllerTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("should get all addresses")
     void readAllAddresses_shouldGetAllAddresses() throws Exception {
         //given
@@ -79,15 +76,40 @@ class AddressControllerTest {
                 "2");
         addressRepository.save(first);
         addressRepository.save(second);
-
-        var mockAddressRepository = mock(AddressRepository.class);
-
-        //when
-        when(mockAddressRepository.findAll()).thenReturn(Lists.newArrayList(first, second));
-
-        //then
+        //when+then
         this.mockMvc.perform(MockMvcRequestBuilders.get("/addresses"))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().is(200))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].city", Matchers.is("Warszawa")));
+
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("should post address")
+    void addAddress() throws Exception {
+        //given
+        Address adr = new Address(
+                3,
+                "Gdansk",
+                "Pomorskie",
+                "12-345",
+                "Gdanska",
+                "3");
+        //when+then
+        mockMvc.perform(MockMvcRequestBuilders.post("/addresses/add")
+                .content(asJsonString(adr))
+                .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(201))
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
